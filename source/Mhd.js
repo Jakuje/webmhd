d45b131a340ff73c687d701618869ec756ea5c25
Jakub Jelen <jakuje@gmail.com>
2013-09-10 22:38:46 +0200

    Working version without help and preferences

--- source/Mhd.js
+++ source/Mhd.js
@@ -1,22 +1,22 @@
 /**
  * Vyhledání nejbližší zastávky podle GPS ???
- * Oblíbené
  */
+PREFS_COOKIE = "webHMDprefs";
 
 enyo.kind({
 	name: "Mhd",
 	kind: enyo.VFlexBox,
 	components: [
 		{kind: "PageHeader", components: [
-			{kind: "Input", name: "zastavka", onchange: "storePrefs", flex: 1, components: [
-					{ content: "Zastávka", className: "enyo-label"},
+			{kind: "Input", name: "zastavka", onchange: "zastavkaChanged",
+				hint: "Zastávka", flex: 1, selectAllOnFocus: true, components: [
 					{kind: "ListSelector", value: "", hideItem: true,
 						hideArrow: true, name: "selector", popupAlign: 'left',
 						onChange: "listSelected", items: []}
 				]
 			},
 			{kind: "ActivityButton", content: "Hledej", name: "search",
-				className: "enyo-button-affirmative", onclick: "buttonPressed"}
+				className: "enyo-button-affirmative", onclick: "searchPressed"}
 			
 			//{content: "Page Header"}
 		]},
@@ -37,12 +37,20 @@ enyo.kind({
 			}
 			]}
 		]},
-		/*{kind: "Toolbar", components: [
-			
-		]}*/
+		{kind: "Toolbar", components: [
+			{kind: "ToolButton", name: 'addFavorite', icon: "images/menu-icon-add.png",
+				onclick: "addFavorites", toggling: true},
+			{kind: "Spacer"},
+			{kind: "ToolButton", icon: "images/toaster-icon-bookmarks.png", onclick: "showFavorites"},
+			{kind: "ListSelector", name: "favorites", hideItem: true, hideArrow: true,
+				onSelect: "listSelected", items: [], },
+			{kind: "ToolButton", icon: "images/toaster-icon-history.png", onclick: "showHistory"},
+			{kind: "ListSelector", name: "history", hideItem: true, hideArrow: true,
+				onSelect: "listSelected", items: [], value: undefined}
+		]},
 		{kind: "AppMenu", components: [ // In chromium: CTRL + `
 			{kind: "EditMenu"},
-			{caption: "Refresh", name: "menuRefresh", onclick: "doRefresh"},
+			{caption: "Add Favorites", name: "menuRefresh", onclick: "doRefresh"},
 			{caption: "Preferences", onclick: "showPreferences"},
 			{caption: "Help", onclick: "showHelp"}
 		]},
@@ -79,38 +87,110 @@ enyo.kind({
 		},
 
 	],
+	
+	create: function() {
+		this.inherited(arguments);
+		this.$.zastavka.setValue(this.getPrefs('current'));
+		this.updateUI();
+	},
+	ready: function(){
+		this.$.zastavka.forceFocus();
+	},
+	updateUI: function(){
+		current = this.$.zastavka.getValue();
+		this.favorites = this.getPrefs('favorites');
+		this.$.favorites.setItems(this.favorites);
+		this.history = this.getPrefs('history');
+		this.$.history.setItems(this.history);
+		if( this.favorites.indexOf(current) !== -1 ){
+			this.$.addFavorite.setDepressed(true);
+		} else {
+			this.$.addFavorite.setDepressed(false);
+		}
+
+	},
+	
+	zastavkaChanged: function(inSender){
+		this.setPrefs("current", inSender.getValue());
+	},
+	
+	favorites: [],
+	history: [],
+	
+	addFavorites: function(inSender, inEvent){
+		var current = this.$.zastavka.getValue();
+		if( inSender.depressed ){
+			this.favorites.push(current);
+		} else {
+			var index = this.favorites.indexOf(current);
+			this.favorites.splice(index, 1);
+		}
+		this.setPrefs("favorites", this.favorites);
+		this.updateUI();
+	},
+	
+	addHistory: function(zastavka){
+		this.history.push(zastavka);
+		if( this.history.length > 10 ){
+			this.history.pop();
+		}
+		this.setPrefs("history", this.history);
+		this.updateUI();
+	},
+	
+	showFavorites: function(){
+		if( this.favorites.length > 0 ){
+			this.$.favorites.openPopup();
+		}
+	},
+	
+	showHistory: function(){
+		if( this.history.length > 0 ){
+			this.$.history.openPopup();
+		}
+	},
+	
 	odjezdy: [],
+	
 	showPopup: function(message){
 		this.$.messageScroller.setScrollTop(0);
 		this.$.errorText.setContent(message);
 		this.$.errorMessage.openAtCenter();
 	},
+	
 	closePopup: function(inSender, inEvent) {
 		this.$.errorMessage.close();
 	},
-	listSelected: function(inSender, inValue, inOldValue){
+	
+	listSelected: function(inSender, inValue){
 		this.$.search.setActive(true);
 		this.$.search.setDisabled(true);
 		
-		title = this.$.selector.popup.fetchItemByValue(inValue);
-		this.$.zastavka.setValue(title.caption);
-		
-		this.doRequest(inValue);
+		title = inSender.items[inValue];
+		this.$.zastavka.setValue(title);
+		this.doRequest("index.php");
+		this.updateUI();
+		return false;
 	},
-	buttonPressed: function(){
+	
+	searchPressed: function(){
 		this.$.search.setActive(true);
 		this.$.search.setDisabled(true);
 		
 		this.doRequest('index.php');
 	},
+	
 	doRequest: function(url){
 		search = this.$.zastavka.getValue();
 		xmlhttp=new XMLHttpRequest();
 		xmlhttp.open('post', 'http://www.reinto.cz/projekty/mhd/'+url, true);
 		xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
 		xmlhttp.onreadystatechange = this.doneRequest.bind(this);
-		xmlhttp.send('stop='+search+'&odeslat=Hledej');
+		xmlhttp.send('stop='+encodeURIComponent(search)+'&odeslat=Hledej');
+		
+		// timeout
 	},
+	
 	doneRequest: function(r){
 		if (xmlhttp.readyState==4 && xmlhttp.status==200) {
 			var reply = xmlhttp.responseText;
@@ -127,8 +207,13 @@ enyo.kind({
 					}
 				}
 				
-				this.$.selector.setItems(items);
-				this.$.selector.openPopup();
+				if( items.length > 0 ){
+					// mame alespon jednu zastavku na vyber v seznamu
+					this.$.selector.setItems(items);
+					this.$.selector.openPopup();
+				} else {
+					this.showPopup("Zastávka nenalezena.");
+				}
 			} else if( reply.indexOf('<div id="telo">') !== -1) {
 				// odjezdy
 				if( reply.indexOf('<div class="cislo">') !== -1) {
@@ -148,11 +233,19 @@ enyo.kind({
 					// nejsou vysledky: neexistuji odjezdy, nebo je nejaky vypadek
 					this.showPopup("Služba vrátila prázný seznam výsledků. Z dané zastávky nejede žádný spoj, nebo je web IDS JMK nedostupný.");
 				}
+				var zastavka = this.$.zastavka.getValue();
+				if( this.favorites.indexOf(zastavka) !== -1 ){
+					this.$.addFavorite.setDepressed(true);
+				} else {
+					this.$.addFavorite.setDepressed(false);
+				}
+				this.addHistory(zastavka);
 			}
 			this.$.search.setActive(false);
 			this.$.search.setDisabled(false);
 		}
 	},
+	
 	listGetItem: function(inSender, inIndex){
 		if (inIndex < this.odjezdy.length) {
 			if (this.$.list) {
@@ -164,5 +257,32 @@ enyo.kind({
 			return true;
 		}
 		return false;
-	}
+	},
+	prefs: null,
+	
+	default_prefs: {"favorites": [], "current": "", "history": []},
+	
+	getPrefs: function(key){
+		if( !this.prefs ){
+			try {
+				this.prefs = enyo.json.parse( enyo.getCookie(PREFS_COOKIE) );
+			} catch(err) {
+				this.prefs = this.default_prefs;
+				this.setPrefs();
+			}
+		}
+		if( typeof this.prefs[key] != 'undefined' ){
+			return this.prefs[key];
+		} else {
+			return this.default_prefs[key];
+		}
+	},
+	
+	setPrefs: function(key, value){
+		if( key ){
+			this.prefs[key] = value;
+		}
+		enyo.setCookie(PREFS_COOKIE, enyo.json.stringify(this.prefs));
+	},
+	
 });
